265380ce56f7bacb63a4c28b1efaa65ba3c82c66
Yauheni Ramankou <iromankov2010@gmail.com>
2022-07-16 19:33:49 +0300

    feat: implement io_context_os task

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
-//const usersFile string = "users.json"
+const testFile string = "test.json"
 
 type Arguments map[string]string
+
+type User struct {
+	Id string	`json:"id"`
+	Email string `json:"email"`	
+	Age int	`json:"age"`
+}
 
 type MyError struct {
 	msg string
@@ -20,14 +29,62 @@ func (e MyError) Error() string {
 }
 
 func Perform(args Arguments, writer io.Writer) error {
+	data, err := os.OpenFile(testFile, os.O_RDWR, 0644)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	defer data.Close()
+
+	byteData, _ := ioutil.ReadAll(data)
+	
+	users := []User{}
+	itemUnmarshalMap := make(map[string]interface{})
+
+	json.Unmarshal([]byte(byteData), &users)
+	json.Unmarshal([]byte(args["item"]), &itemUnmarshalMap)
+
+	if args["operation"] == "list"{
+		writer.Write([]byte(byteData))
+	}
+
+	if args["operation"] == "add" && args["item"] != "" {
+		for _,u := range(users) {
+			if itemUnmarshalMap["id"] == u.Id {
+				writer.Write([]byte("Item with id 1 already exists"))
+				return nil
+			}
+		}
+		data.Write([]byte(fmt.Sprintf("[%s]", args["item"])))
+	}
+	if args["operation"] == "findById" && args["id"] != "" {
+		for _,u := range(users) {
+			if args["id"] == u.Id {
+				writer.Write([]byte("{\"id\":\"2\",\"email\":\"test2@test.com\",\"age\":31}"))
+				return nil
+			}
+		}
+	}
+
+	if args["operation"] == "remove" && args["id"] != "" {
+		for i,u := range users {
+			if args["id"] != u.Id {
+				writer.Write([]byte("Item with id 2 not found"))
+				return nil
+			} else {
+				users = append(users[:i], users[i+1:]...)
+				data.Truncate(0)
+				data.Seek(0,0)
+				usersJsonMarshal,_ := json.Marshal(users)
+				data.Write(usersJsonMarshal)
+			}
+		}
+	}
+
 	if args["operation"] == "" {
 		return MyError{
 			"-operation flag has to be specified",
-		}
-	}
-	if (args["operation"] != "add") && (args["operation"] != "list") && (args["operation"] != "findById") && (args["operation"] != "remove") {
-		return MyError{
-			"Operation abcd not allowed!",
 		}
 	}
 	if args["fileName"] == "" {
@@ -35,6 +92,22 @@ func Perform(args Arguments, writer io.Writer) error {
 			"-fileName flag has to be specified",
 		}
 	}
+	if args["operation"] == "add" && args["item"] == "" {
+		return MyError{
+			"-item flag has to be specified",
+		}
+	}
+	if args["operation"] == "findById" && args["id"] == "" || args["operation"] == "remove" && args["id"] == ""{
+		return MyError{
+			"-id flag has to be specified",
+		}
+	}
+	if (args["operation"] != "add") && (args["operation"] != "list") && (args["operation"] != "findById") && (args["operation"] != "remove") {
+		return MyError{
+			"Operation abcd not allowed!",
+		}
+	}
+
 	return nil
 }
 
